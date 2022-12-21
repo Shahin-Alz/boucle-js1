f650a19a32810a691c0b264b2da428c5d949700e
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-21 14:00:48 +0100

    ajout de la premiere parti exo

---
+++ public/js/main.js
@@ -0,0 +1,95 @@
+// n1
+
+// let i= 0
+
+// for (let i = 0; i < 10; i= i+1) {
+//     console.log(i);
+
+// }
+
+
+
+// N2 
+
+// let i = 10;
+
+// while (i > 0) {
+
+//     i = i - 1
+//     console.log("bonjour" + i);
+
+// }
+
+
+
+
+
+// N3 pas fonctionnel + pc qui chauffe inshallah jfais pas de connerie
+
+// let i = 20;
+
+// do {
+//     i = i % 2;
+
+// } 
+// while (i < 20);
+
+// console.log(i);
+
+
+// let result = '';
+// let i = 0;
+
+// do {
+//     i = i + 1;
+//     result = result + i;
+// } while (i % 20);
+
+// console.log(resul2);
+
+
+
+
+// n4
+
+// let tableau = ['un', 'deux', 'trois'];
+
+// for (const element of tableau) {
+//     console.log(element);
+// }
+
+
+
+// n5
+// let tableau = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let addition = 0;
+
+// for (let i = 0; i < tableau.length; i++) {
+//     addition += tableau[i];
+// }
+// console.log(addition);
+
+
+
+// n6 pas reussi
+
+// let uno = +prompt("saisi un nombre")
+
+// while (uno != 1) {
+//     uno++;
+//     console.log(Math.sign(1));
+// }
+
+
+// n7 
+
+// let i= 0;
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// n8
